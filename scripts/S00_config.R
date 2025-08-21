fcba17d593e1824c95ae9f835623c7b6638da89e
katemasters <km923@cam.ac.uk>
2025-08-21 17:10:36 +0100

    palette genomes

---
+++ scripts/S00_config.R
@@ -0,0 +1,7 @@
+
+genotype_palette <- c("tT" = "red", 
+                      "mT" = "orange", 
+                      "tM" = "green", 
+                      "mM" = "blue")
+
+genotype <- c("tT", "mT", "tM", "mM")
